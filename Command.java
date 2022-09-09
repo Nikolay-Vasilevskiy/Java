fedc35af956e0a874e11a50852f52c44f4bd7c6b
Oksana <caps14@gmail.com>
2022-09-10 00:16:58 +0800

    Command.java

--- Command.java
+++ Command.java
@@ -1,35 +1,7 @@
-// Показать набор команд для превращения числа а в число b
+// Домашнее задание № 1 Показать набор команд для превращения числа а в число b
 
-// import java.util.Scanner;
-
-public class Command {
-
-    
-{
-
-    
-}  
-
-    // public static void main(String[] args){
-    // int a,b;
-    // System.out.println("Введите числа A и B");
-    // Scanner iScanner = new Scanner(System.in);
-
-    // a = iScanner.nextInt(); // a = 7
-    // b = iScanner.nextInt(); // b = 5
-
-    // System.out.println("Изначальные числа \na = "+a+"\nb = " +b);
-
-    // a = a + b; // присваеваем значение a что оно имеет теперь в себе a и b || a =
-    // 7 + 5 = 12
-    // b = a - b; // присваеваем b значение ((a + b) - b) || b = 12 - 5 = 7
-    // a = a - b; // новое значение a = ((a + b) - ((a + b) - b)) || a = 12 - 7 = 5
-
-    // System.out.println("Перевернутые числа \na = "+a+"\nb = "+b); // результат
-    // перемены
-    // iScanner.close();
-
-    // }
+public class Command
+ { 
     // На вход подаётся два числа (a, b). Есть две команды
     // - команда 1 (к1): увеличить в с раза, а умножается на c
     // - команда 2 (к2): увеличить на d, к a прибавляется d
