2704cf0fdf59ee03f1308bab2729a803835c9a4f
Oksana <caps14@gmail.com>
2022-09-10 22:05:52 +0800

    Treugolnik.java

---
+++ Treugolnik.java
@@ -0,0 +1,23 @@
+//Домашнее задание № 2 Написать программу вычисления n-ого треугольного числа
+import java.util.Scanner;
+
+public class Treugolnik { public static void main(String[] args) {  
+
+    Scanner can = new Scanner(System.in);
+    System.out.println("Введите число n, для вычисления n-ого треугольного числа:");
+
+        int n = can.nextInt();
+
+        if (n < 0) {
+            System.out.println("Для отрицательных чисел треугольное число посчитать не получится.");
+        } else {
+            System.out.println("Для n = " + n + " треугольное число = " + triangularNumber(n));
+        }
+    }
+
+    public static int triangularNumber(int n) {
+        int result =   (n * (n + 1)) / 2;        
+        
+        return (result);
+    }
+}
